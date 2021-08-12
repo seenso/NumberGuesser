1c18ff977b8f7169fe0822fdf8d3eee6b914f74a
seenso <>
2021-08-11 16:04:28 -0800

    Implemented generateTarget(), compareGuesses(), and working on updateScore()

--- script.js
+++ script.js
@@ -3,7 +3,27 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
-
-const test = () => {
-    
+const generateTarget = () => {
+  //gen new secret target number between 0 and 9
+  return Math.floor(Math.random()*10);
 };
+
+const compareGuesses = (human, computer, target) => {
+  let humanDiff = Math.Abs(humanDiff-target);
+  let computerDiff = Math.Abs(humanDiff-target);
+  let humanWon = false;
+  if (humanDiff < computerDiff) {
+    humanWon = true;
+  } else if (humanDiff > computerDiff) {
+    humanWon = false;
+  } else {
+    humanWon = true; //if tie, human wins. 
+  }
+
+  return humanWon;
+};
+
+const updateScore = (winner) => {
+    let humanScore = 0;
+    let computerScore = 0;x
+}
